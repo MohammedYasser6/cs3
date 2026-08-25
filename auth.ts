868506d099db500184c0c1ded28e80830538da29
mohammed <moonson2004@gmail.com>
2026-08-25 23:20:21 +0300

    Explicitly map Auth providers

--- auth.ts
+++ auth.ts
@@ -2,11 +2,19 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma"; // Adjust path if your lib folder isn't at the root
+import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  // We use JWT here so your proxy.ts (middleware) can still read the session on the Edge network
   session: { strategy: "jwt" },
-  providers: [GitHub, Google],
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
 });
